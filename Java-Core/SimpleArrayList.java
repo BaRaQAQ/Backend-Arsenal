ecea15a0e026ec1a093efc51e75e5e4aa2fe26f9
BaRaQAQ <1227060982@qq.com>
2025-02-25 14:56:50 +0800

    更改

---
+++ Java-Core/SimpleArrayList.java
@@ -0,0 +1,8 @@
+/**
+ * @program: Backend-Arsenal/Java-Core
+ * @description: 实现精简版ArrayList（重点：System.arraycopy优化思路）
+ * @author: Selina_Xie
+ * @create: 2025-02-25
+ */
+public class SimpleArrayList {
+}
